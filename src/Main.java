d1c26302e4444f3b90c205675a8f1505f0d08714
F_Aleksey <2013polina89@gmail.com>
2026-06-30 23:44:21 +0400

    Правки под условия

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int ticketprice = 5875;
-        int miles = ticketprice / 20;
+        int rublesPerMil = 20;
+        int totalmile = ticketprice / rublesPerMil;
 
         System.out.println("Стоимость билета " + ticketprice + " руб.");
-        System.out.println("Будет начисленно " + miles + " миль.");
+        System.out.println("Будет начисленно " + totalmile + " миль.");
     }
 }
