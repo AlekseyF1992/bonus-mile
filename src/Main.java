6e7e35ad6ba3d73e3cf6caf6c05c14a0b057e1c4
F_Aleksey <2013polina89@gmail.com>
2026-06-30 23:26:20 +0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 5875;
+        int miles = ticketprice / 20;
+
+        System.out.println("Стоимость билета " + ticketprice + " руб.");
+        System.out.println("Будет начисленно " + miles + " миль.");
+    }
+}
